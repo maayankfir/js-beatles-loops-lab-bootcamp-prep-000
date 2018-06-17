acecdb63c140e92fbea46addbbbd943a4fb25b25
maayankfir <mayankfir@gmail.com>
2018-06-17 09:00:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push ("${musicians[i]} plays ${instruments[i]}");
+  }
+  return array;
+}
+
+function johnLennonFacts (facts) {
+  var newFacts = []
+  var i = 0
+  while (i < facts.length ) {
+    newFacts.push ("${facts[i]} !!!")
+    i ++
+  }
+    return newFacts
+  }
+
+  function iLoveTheBeatles (number) {
+    var emptyarray = []
+    do {
+      emptyarray.push ("I love the Beatles!")
+        number ++
+    } while (number < 15) {
+
+    }
+      return emptyarray
+    }
